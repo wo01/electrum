1e2a8ced9723d224ad3d2c69f3d99a31cb70844b
WO <wo@kotocoin.info>
2018-02-09 17:47:17 +0900

    Change: electrum.png -> kotocolot.png

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ if platform.system() in ['Linux', 'FreeBSD', 'DragonFly']:
             usr_share = os.path.expanduser('~/.local/share')
     data_files += [
         (os.path.join(usr_share, 'applications/'), ['electrum.desktop']),
-        (os.path.join(usr_share, 'pixmaps/'), ['icons/electrum.png'])
+        (os.path.join(usr_share, 'pixmaps/'), ['icons/kotocolor.png'])
     ]
 
 setup(
