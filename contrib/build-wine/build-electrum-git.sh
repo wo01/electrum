84effe7905c24337f89828d579b789ca88bae8a4
WO <wo@kotocoin.info>
2018-02-09 17:18:57 +0900

    Oops: fix verison number

--- contrib/build-wine/build-electrum-git.sh
+++ contrib/build-wine/build-electrum-git.sh
@@ -60,7 +60,7 @@ if [ ! -z "$1" ]; then
     git checkout $1
 fi
 
-VERSION=3.1.0#`git describe --tags`
+VERSION=`git describe --tags`
 echo "Last commit: $VERSION"
 find -exec touch -d '2000-11-11T11:11:11+00:00' {} +
 popd
