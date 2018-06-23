179cdcdc61647a6ad5875f3ea36e2517d344add3
WO <wo@kotocoin.info>
2018-06-24 08:13:04 +0900

    Change name for Trezor

--- plugins/trezor/trezor.py
+++ plugins/trezor/trezor.py
@@ -173,7 +173,7 @@ class TrezorPlugin(HW_PluginBase):
         return client
 
     def get_coin_name(self):
-        return "Testnet" if constants.net.TESTNET else "Bitcoin"
+        return "Testnet" if constants.net.TESTNET else "Koto"
 
     def initialize_device(self, device_id, wizard, handler):
         # Initialization method
