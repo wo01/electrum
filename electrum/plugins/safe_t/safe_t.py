66f86e86f40348ae3cbb114714afba47bddb6023
脇山P <wakiyamap@gmail.com>
2018-08-24 12:13:28 +0900

    Fix coinname used with safe_t

--- electrum/plugins/safe_t/safe_t.py
+++ electrum/plugins/safe_t/safe_t.py
@@ -163,7 +163,7 @@ class SafeTPlugin(HW_PluginBase):
         return client
 
     def get_coin_name(self):
-        return "Testnet" if constants.net.TESTNET else "Bitcoin"
+        return "Testnet" if constants.net.TESTNET else "Koto"
 
     def initialize_device(self, device_id, wizard, handler):
         # Initialization method
