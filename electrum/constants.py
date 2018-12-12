0ec278e412330539702f794e62e01ad9b09d0bec
WO <wo@kotocoin.info>
2018-12-12 19:25:30 +0900

    Delete segwit headers

--- electrum/constants.py
+++ electrum/constants.py
@@ -58,17 +58,17 @@ class BitcoinMainnet(AbstractNet):
 
     XPRV_HEADERS = {
         'standard':    0x0488ade4,  # xprv
-        'p2wpkh-p2sh': 0x049d7878,  # yprv
-        'p2wsh-p2sh':  0x0295b005,  # Yprv
-        'p2wpkh':      0x04b2430c,  # zprv
-        'p2wsh':       0x02aa7a99,  # Zprv
+#        'p2wpkh-p2sh': 0x049d7878,  # yprv
+#        'p2wsh-p2sh':  0x0295b005,  # Yprv
+#        'p2wpkh':      0x04b2430c,  # zprv
+#        'p2wsh':       0x02aa7a99,  # Zprv
     }
     XPUB_HEADERS = {
         'standard':    0x0488b21e,  # xpub
-        'p2wpkh-p2sh': 0x049d7cb2,  # ypub
-        'p2wsh-p2sh':  0x0295b43f,  # Ypub
-        'p2wpkh':      0x04b24746,  # zpub
-        'p2wsh':       0x02aa7ed3,  # Zpub
+#        'p2wpkh-p2sh': 0x049d7cb2,  # ypub
+#        'p2wsh-p2sh':  0x0295b43f,  # Ypub
+#        'p2wpkh':      0x04b24746,  # zpub
+#        'p2wsh':       0x02aa7ed3,  # Zpub
     }
     BIP44_COIN_TYPE = 0
 
@@ -87,17 +87,17 @@ class BitcoinTestnet(AbstractNet):
 
     XPRV_HEADERS = {
         'standard':    0x04358394,  # tprv
-        'p2wpkh-p2sh': 0x044a4e28,  # uprv
-        'p2wsh-p2sh':  0x024285b5,  # Uprv
-        'p2wpkh':      0x045f18bc,  # vprv
-        'p2wsh':       0x02575048,  # Vprv
+#        'p2wpkh-p2sh': 0x044a4e28,  # uprv
+#        'p2wsh-p2sh':  0x024285b5,  # Uprv
+#        'p2wpkh':      0x045f18bc,  # vprv
+#        'p2wsh':       0x02575048,  # Vprv
     }
     XPUB_HEADERS = {
         'standard':    0x043587cf,  # tpub
-        'p2wpkh-p2sh': 0x044a5262,  # upub
-        'p2wsh-p2sh':  0x024289ef,  # Upub
-        'p2wpkh':      0x045f1cf6,  # vpub
-        'p2wsh':       0x02575483,  # Vpub
+#        'p2wpkh-p2sh': 0x044a5262,  # upub
+#        'p2wsh-p2sh':  0x024289ef,  # Upub
+#        'p2wpkh':      0x045f1cf6,  # vpub
+#        'p2wsh':       0x02575483,  # Vpub
     }
     BIP44_COIN_TYPE = 1
 
