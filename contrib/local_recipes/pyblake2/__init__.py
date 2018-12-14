c5d36a0a9680cee08f4ed0c1003d4fc3a37f3a34
WO <wo@ko-to.org>
2018-12-14 23:42:27 +0900

    Fix local_recipes to install shared lib

--- contrib/local_recipes/pyblake2/__init__.py
+++ contrib/local_recipes/pyblake2/__init__.py
@@ -1,10 +1,20 @@
 from pythonforandroid.recipe import CythonRecipe
-
+from pythonforandroid.toolchain import current_directory
+import shutil
+import glob
 
 class Pyblake2bRecipe(CythonRecipe):
 
     version = '1.1.2'
     url = 'https://github.com/dchest/pyblake2/archive/v{version}.tar.gz'
     depends = ['setuptools']
+
+    def build_arch(self, arch):
+        super(Pyblake2bRecipe, self).build_arch(arch)
+
+        env = self.get_recipe_env(arch)
+        with current_directory(self.get_build_dir(arch.arch)):
+            so_file = glob.glob('./build/lib*/*so')
+            shutil.copyfile(so_file[0], self.ctx.get_libs_dir(arch.arch) + "/pyblake2.so")
 
 recipe = Pyblake2bRecipe()
--- contrib/local_recipes/yescrypt/__init__.py
+++ contrib/local_recipes/yescrypt/__init__.py
@@ -1,5 +1,7 @@
 from pythonforandroid.recipe import CythonRecipe
-
+from pythonforandroid.toolchain import current_directory
+import shutil
+import glob
 
 class YescryptRecipe(CythonRecipe):
 
@@ -7,5 +9,13 @@ class YescryptRecipe(CythonRecipe):
 
     patches = ['./setup.patch']
     depends = ['setuptools']
+
+    def build_arch(self, arch):
+        super(YescryptRecipe, self).build_arch(arch)
+
+        env = self.get_recipe_env(arch)
+        with current_directory(self.get_build_dir(arch.arch)):
+            so_file = glob.glob('./build/lib*/*so')
+            shutil.copyfile(so_file[0], self.ctx.get_libs_dir(arch.arch) + "/yescrypt.so")
 
 recipe = YescryptRecipe()
