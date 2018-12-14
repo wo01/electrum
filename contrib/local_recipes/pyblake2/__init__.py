c677aee47ba04e12270f326ee73199c9bbf74f4a
WO <wo@kotocoin.info>
2018-12-15 08:35:02 +0900

    FIx Android build No longer needed -DHAVE_DECL_BE64ENC for yescrypt

--- contrib/local_recipes/pyblake2/__init__.py
+++ contrib/local_recipes/pyblake2/__init__.py
@@ -9,12 +9,12 @@ class Pyblake2bRecipe(CythonRecipe):
     url = 'https://github.com/dchest/pyblake2/archive/v{version}.tar.gz'
     depends = ['setuptools']
 
-    def build_arch(self, arch):
-        super(Pyblake2bRecipe, self).build_arch(arch)
+#    def build_arch(self, arch):
+#        super(Pyblake2bRecipe, self).build_arch(arch)
 
-        env = self.get_recipe_env(arch)
-        with current_directory(self.get_build_dir(arch.arch)):
-            so_file = glob.glob('./build/lib*/*so')
-            shutil.copyfile(so_file[0], self.ctx.get_libs_dir(arch.arch) + "/pyblake2.so")
+#        env = self.get_recipe_env(arch)
+#        with current_directory(self.get_build_dir(arch.arch)):
+#            so_file = glob.glob('./build/lib*/*so')
+#            shutil.copyfile(so_file[0], self.ctx.get_libs_dir(arch.arch) + "/pyblake2.so")
 
 recipe = Pyblake2bRecipe()
