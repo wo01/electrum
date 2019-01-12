bfefe46d4e133a381a8ccc3f8d43b7981e3c39b7
WO <wo@kotocoin.info>
2019-01-12 22:17:38 +0900

    Bug Fix: sweep function

--- electrum/gui/qt/main_window.py
+++ electrum/gui/qt/main_window.py
@@ -1646,7 +1646,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
         on_success = run_hook('tc_sign_wrapper', self.wallet, tx, on_success, on_failure) or on_success
         if self.tx_external_keypairs:
             # can sign directly
-            task = partial(Transaction.sign, tx, self.tx_external_keypairs, self.wallet)
+            task = partial(Transaction.sign, tx, self.tx_external_keypairs)
         else:
             task = partial(self.wallet.sign_transaction, tx, password)
         msg = _('Signing transaction...')
