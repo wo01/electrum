5cf2aff723dd83283faa3106b947735e8c69b9f2
WO <wo@kotocoin.info>
2019-07-10 15:34:38 +0900

    set setWindowIcon to kotocolor_back

--- electrum/gui/qt/__init__.py
+++ electrum/gui/qt/__init__.py
@@ -103,7 +103,7 @@ class ElectrumGui(Logger):
         self.efilter = OpenFileEventFilter(self.windows)
         self.app = QElectrumApplication(sys.argv)
         self.app.installEventFilter(self.efilter)
-        self.app.setWindowIcon(read_QIcon("electrum.png"))
+        self.app.setWindowIcon(read_QIcon("kotocolor_back.ico"))
         # timer
         self.timer = QTimer(self.app)
         self.timer.setSingleShot(False)
